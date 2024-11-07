a0677a6440cbb79e8ae0fcb7322bc92ffb6031f6
Matheus Reato <matheusreato13@gmail.com>
2024-11-07 15:35:32 -0300

    Obtendo um usuario por id

--- src/main/java/com/matheusreato/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusreato/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.matheusreato.workshopmongo.services;
 
 import com.matheusreato.workshopmongo.domain.User;
 import com.matheusreato.workshopmongo.repositories.UserRepository;
+import com.matheusreato.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
